35b3913ca34f9ebb7e58a650795a3a23ea8504b6
exercice <ininahazwedancile8@gmail.com>
2024-11-25 16:05:19 +0000

    mise a jour avec derniere modification

--- java.js
+++ java.js
@@ -13,6 +13,18 @@ const phrases = [
     currentIndex = (currentIndex + 1) % phrases.length; 
   }
   
-des
-  setInterval(changePhrase, 4000);
+
+ 
+const reponse=await fetch("pieces-autos.json");
+const pieces =await reponse.json();
+const article= pieces [0];
+
+
+imageElement.src = article.image;
+const nomElement = document.createElement("h2");
+nomElement.innerText = article.nom;
+const prixElement = document.createElement("p");
+prixElement.innerText = `Prix: ${article.prix} €`;
+const categorieElement = document.createElement("p");
+categorieElement.innerText = article.categorie;
   
